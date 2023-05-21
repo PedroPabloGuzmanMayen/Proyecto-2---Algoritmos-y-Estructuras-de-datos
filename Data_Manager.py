1ccf4f673acbf13a9e05d1e191f7df4a6c2b8302
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2023-05-21 00:01:44 -0600

    Data manager

--- Data_Manager.py
+++ Data_Manager.py
@@ -1,7 +1,45 @@
 from neo4j import GraphDatabase
+import numpy as np
 
 class Data_Manager:
-     def __init__(self):
-        pass
-     def getUserData(self):
-         pass
+   def __init__(self):
+      self.driver = GraphDatabase.driver("neo4j+s://b0a9a659.databases.neo4j.io", auth=("neo4j", "n9_qBLezCVCaPJaivvf08qdDk47l4UGKaTx1x_d5QDk"))
+      self.usernames =[]
+      self.passwords =[]
+      self.Career_feature = np.empty((0, 0))
+      self.career_names = []
+      self.feature_names =[]
+   def getLogInData(self):
+      with self.driver.session() as session:
+         result = session.run("MATCH (u:User) RETURN u.username AS username, u.password AS password")
+
+         for record in result:
+            self.usernames.append(record["username"])
+            self.passwords.append(record["password"])
+
+   def getCareer_feature(self):
+        with self.driver.session() as session:
+         result = session.run("MATCH (c:Career)-[r:HAS_TAG]->(f:Feature) RETURN c.name AS career_name, f.name AS feature_name, r.Strong AS strong")
+
+         for record in result:
+            career_name = record["career_name"]
+            feature_name = record["feature_name"]
+            strong = record["strong"]
+
+            if career_name not in self.career_names:
+                self.career_names.append(career_name)
+
+            if feature_name not in self.feature_names:
+                self.feature_names.append(feature_name)
+
+            career_index = self.career_names.index(career_name)
+            feature_index = self.feature_names.index(feature_name)
+
+            if career_index >= self.Career_feature.shape[0]:
+                self.Career_feature = np.pad(self.Career_feature, ((0, career_index - self.Career_feature.shape[0] + 1), (0, 0)), mode='constant')
+            if feature_index >= self.Career_feature.shape[1]:
+                self.Career_feature = np.pad(self.Career_feature, ((0, 0), (0, feature_index - self.Career_feature.shape[1] + 1)), mode='constant')
+
+            self.Career_feature[career_index][feature_index] = strong
+
+
--- Main.py
+++ Main.py
@@ -1,57 +1,19 @@
+from Data_Manager import Data_Manager
+data_manager = Data_Manager()
+data_manager.getLogInData()
+data_manager.getCareer_feature()
 
-import numpy as np
-from neo4j import GraphDatabase
+# Access the usernames, passwords, career-feature matrix, career names, and feature names
+usernames = data_manager.usernames
+passwords = data_manager.passwords
+career_feature = data_manager.Career_feature
+career_names = data_manager.career_names
+feature_names = data_manager.feature_names
 
-# Connect to the Neo4j database
-driver = GraphDatabase.driver("neo4j+s://b0a9a659.databases.neo4j.io", auth=("neo4j", "n9_qBLezCVCaPJaivvf08qdDk47l4UGKaTx1x_d5QDk"))
-
-# Define the query to retrieve the matrix and node names
-query = """
-MATCH (c:Career)-[r:HAS_TAG]->(f:Feature)
-RETURN c.name AS career_name, f.name AS feature_name, r.Strong AS strong
-"""
-
-# Execute the query and retrieve the results
-with driver.session() as session:
-    result = session.run(query)
-
-    # Initialize an empty matrix and arrays
-    career_names = []
-    feature_names = []
-    matrix = np.empty((0, 0))
-
-    # Iterate over the result and populate the matrix and arrays
-    for record in result:
-        career_name = record["career_name"]
-        feature_name = record["feature_name"]
-        strong = record["strong"]
-
-        if career_name not in career_names:
-            career_names.append(career_name)
-
-        if feature_name not in feature_names:
-            feature_names.append(feature_name)
-
-        # Find the index of the career and feature in the arrays
-        career_index = career_names.index(career_name)
-        feature_index = feature_names.index(feature_name)
-
-        # Expand the matrix size if needed
-        if career_index >= matrix.shape[0]:
-            matrix = np.pad(matrix, ((0, career_index - matrix.shape[0] + 1), (0, 0)), mode='constant')
-        if feature_index >= matrix.shape[1]:
-            matrix = np.pad(matrix, ((0, 0), (0, feature_index - matrix.shape[1] + 1)), mode='constant')
-
-        # Assign the strong value to the corresponding position in the matrix
-        matrix[career_index][feature_index] = strong
-
-# Print the matrix, career names, and feature names
-print("Matrix:")
-print(matrix)
-
-print("Career names:", career_names)
-print("Feature names:", feature_names)
-
-print()
-print()
-print(matrix[2,9])
+# Print the lists and matrix
+print("Usernames:", usernames)
+print("Passwords:", passwords)
+print("Career-Feature Matrix:")
+print(career_feature)
+print("Career Names:", career_names)
+print("Feature Names:", feature_names)
