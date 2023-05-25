d8a1099a83dd287b11c1d28e0829a393390c807d
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2023-05-25 11:04:54 -0600

    Mejoras

---
+++ UserCreation.py
@@ -0,0 +1,55 @@
+import tkinter as tk
+from tkinter import messagebox
+import numpy as np
+
+
+class UserCreationGUI:
+    def __init__(self, array, np_array):
+        self.array = array
+        self.np_array = np_array
+        self.index = 0
+
+        self.window = tk.Tk()
+        self.window.title("User Creation")
+        self.window.geometry("300x200")
+        self.window.configure(bg="#F4F4F4")
+
+        self.label_message = tk.Label(self.window, text="Creando tu usuario", font=("Arial", 16), bg="#F4F4F4", fg="#000000")
+        self.label_message.pack(pady=10)
+
+        line = tk.Frame(self.window, height=2, width=300, bg="#000000")
+        line.pack()
+
+        self.label_array_element = tk.Label(self.window, text=self.array[self.index], font=("Arial", 12), bg="#F4F4F4", fg="#000000")
+        self.label_array_element.pack(pady=10)
+
+        self.entry_value = tk.Entry(self.window)
+        self.entry_value.pack(pady=5)
+
+        self.button_next = tk.Button(self.window, text="Siguiente", command=self.nextClicked, bg="#ECECEC", fg="#000000")
+        self.button_next.pack(pady=10)
+
+        self.window.mainloop()
+
+    def nextClicked(self):
+        value = self.entry_value.get()
+        if value == "":
+            messagebox.showinfo("Error", "Ingresa un valor")
+        elif not value.isnumeric():
+            messagebox.showinfo("Error", "Por favor ingresa un valor numérico")
+        else:
+            self.np_array[self.index] += float(value)
+            self.index += 1
+            if self.index == len(self.array):
+                print("Last element")
+                print(self.np_array)
+                self.window.destroy()
+                
+            else:
+                self.label_array_element.config(text=self.array[self.index])
+                self.entry_value.delete(0, tk.END)
+
+# Example usage
+array = ["Element 1", "Element 2", "Element 3"]
+np_array = np.zeros(len(array))
+gui = UserCreationGUI(array, np_array)
